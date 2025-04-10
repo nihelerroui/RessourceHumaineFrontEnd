22f706558704c9c0bea728cbb880f1f3fbf582f4
Wissal Mechmeche <mechmechwissal@gmail.com>
2025-04-10 17:14:49 +0200

    update dépenses avec store

--- src/app/core/services/depense.service.ts
+++ src/app/core/services/depense.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GenericService } from './generic.service';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +10,5 @@ export class DepenseService extends GenericService<any> {
 
   constructor(protected http: HttpClient) {
     super(http, 'depenses');
-  }
-
-  getFactures(): Observable<any[]> {
-    return this.http.get<any[]>(`${environment.apiUrl}/factures`);
-  }
-
-  getSocietes(): Observable<any[]> {
-    return this.http.get<any[]>(`${environment.apiUrl}/societes`);
-  }
-
-  getUnassignedFactures(): Observable<any[]> {
-    return this.http.get<any[]>(`${environment.apiUrl}/factures/unassigned`);
-  }
-
-  getDepenseDetails(id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 }
