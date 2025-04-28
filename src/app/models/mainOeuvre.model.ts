c7041af5ba49a0e3ce92a6ccd5d10268b896fd14
Wissal Mechmeche <mechmechwissal@gmail.com>
2025-04-28 14:59:59 +0200

    Update mainOeuvre.model.ts

--- src/app/models/mainOeuvre.model.ts
+++ src/app/models/mainOeuvre.model.ts
@@ -1,14 +1,9 @@
+import { Consultant } from "./consultant.models";
+
 export interface MainOeuvre {
   mainoeuvreId?: number;
   coutGlobale: number;
   mois: number;
   annee: number;
-  consultant: {
-    consultantId: number;
-    fullName: string;
-    societe: {
-      societeId: number;
-      name?: string;
-    };
-  };
+  consultant: Consultant ;
 }
